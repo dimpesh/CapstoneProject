9972d141586aa5132b650a75f085623a95149b32
dk_2094 <kdimpesh@gmail.com>
2016-09-10 21:17:04 +0530

    Simple LoginActivity created.

--- app/src/main/java/app/capstoneproject/ui/LoginActivity.java
+++ app/src/main/java/app/capstoneproject/ui/LoginActivity.java
@@ -1,15 +1,47 @@
 package app.capstoneproject.ui;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Toast;
+
 import app.capstoneproject.R;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 
 public class LoginActivity extends AppCompatActivity {
+    private static String TAG="LoginActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        ButterKnife.bind(this);
     }
+
+
+    @OnClick({R.id.signInbutton, R.id.google_button, R.id.forgotClick, R.id.createClick})
+    public void signIn(View v) {
+        switch (v.getId()) {
+            case R.id.signInbutton: {
+                Toast.makeText(this, "Login Button Clicked", Toast.LENGTH_SHORT).show();
+                break;
+            }
+            case R.id.google_button: {
+                Toast.makeText(this, "Google Button Clicked", Toast.LENGTH_SHORT).show();
+                break;
+            }
+            case R.id.forgotClick: {
+                Toast.makeText(this, "Forgot Password Clicked", Toast.LENGTH_SHORT).show();
+                break;
+            }
+            case R.id.createClick: {
+                Toast.makeText(this, "Create Account Clicked", Toast.LENGTH_SHORT).show();
+                break;
+
+            }
+        }
+    }
+
 }

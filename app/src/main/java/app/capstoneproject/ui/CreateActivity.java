7ae38e51cc310b6724d659b4f9de0e63af1288a2
dk_2094 <kdimpesh@gmail.com>
2016-09-12 01:27:25 +0530

    Simple CreateActivity(for  SignUp) Done.

--- app/src/main/java/app/capstoneproject/ui/CreateActivity.java
+++ app/src/main/java/app/capstoneproject/ui/CreateActivity.java
@@ -1,16 +1,50 @@
 package app.capstoneproject.ui;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.EditText;
+
+import java.util.Calendar;
 
 import app.capstoneproject.R;
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class CreateActivity extends AppCompatActivity {
+    @BindView(R.id.create_dob)
+    EditText dob;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create);
         setTitle(getString(R.string.create_activity_title));
+        ButterKnife.bind(this);
     }
+
+    @OnClick(R.id.create_dob_button)
+    public void dateChooser(View v) {
+        DateDialog();
+    }
+
+
+    public void DateDialog() {
+        Calendar mcurrentDate = Calendar.getInstance();
+        int mYear = mcurrentDate.get(Calendar.YEAR);
+        int mMonth = mcurrentDate.get(Calendar.MONTH);
+        int mDay = mcurrentDate.get(Calendar.DAY_OF_MONTH);
+
+        DatePickerDialog mDatePicker;
+        mDatePicker = new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
+            public void onDateSet(DatePicker datepicker, int selectedyear, int selectedmonth, int selectedday) {
+                selectedmonth = selectedmonth + 1;
+                dob.setText("" + selectedday + "/" + selectedmonth + "/" + selectedyear);
+            }
+        }, mYear, mMonth, mDay);
+        mDatePicker.setTitle("Select Date");
+        mDatePicker.show();    }
 }
